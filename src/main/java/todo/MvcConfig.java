633ea0681e8aeeb52f8a5ed50f34565b087694a4
4rch18 <archit11030@iiitd.ac.in>
2018-11-22 15:54:36 +0530

    Reduced to single page application

--- src/main/java/todo/MvcConfig.java
+++ src/main/java/todo/MvcConfig.java
@@ -8,9 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/todo").setViewName("hello");
+        registry.addViewController("/").setViewName("redirect:/task");
+        registry.addViewController("/task").setViewName("task");
         registry.addViewController("/login").setViewName("login");
     }
 
--- src/main/java/todo/WebSecurityConfig.java
+++ src/main/java/todo/WebSecurityConfig.java
@@ -17,7 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
