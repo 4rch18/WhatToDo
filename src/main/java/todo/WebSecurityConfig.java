d158dc6d4f3fbb064963d61fc9a4fe0150508375
4rch18 <archit11030@iiitd.ac.in>
2018-11-22 20:03:47 +0530

    Updated login page

--- src/main/java/todo/WebSecurityConfig.java
+++ src/main/java/todo/WebSecurityConfig.java
@@ -17,6 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+                .antMatchers("/", "/home","/public/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
