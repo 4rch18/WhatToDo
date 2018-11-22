5b78b2151428be10b4061afcec46d645f2924394
Tiru Sharma <tiru@goniyo.com>
2018-11-22 11:11:11 +0530

    Added noargs constructor

--- src/main/java/todo/controller/TaskController.java
+++ src/main/java/todo/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import todo.dto.TaskDTO;
 import todo.model.Task;
 
 @Controller
@@ -12,7 +13,7 @@ public class TaskController {
 
     @GetMapping("/task")
     public String taskForm(Model model) {
-        model.addAttribute("emptyTask",new Task());
+        model.addAttribute("emptyTask",new TaskDTO());
         return "task";
     }
 
--- src/main/java/todo/dto/TaskDTO.java
+++ src/main/java/todo/dto/TaskDTO.java
@@ -3,9 +3,11 @@ package todo.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskDTO {
 
